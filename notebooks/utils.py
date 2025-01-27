e64c5ef30e6aabb318d2e47afd3c31eea4f22507
mm391-030401 <mm391@hdm-stuttgart.de>
2025-01-27 23:11:46 +0100

    Update utils
    Fehler in Kategorisierung Schiefe korrigiert
    Neue Funktion zur Erkennung von Duplikaten

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,5 +1,6 @@
 # Imports
 from scipy.stats import shapiro
+import numpy as np
 
 def hex_to_rgb_normalized(hex_color):
     hex_color = hex_color.lstrip('#')
@@ -12,7 +13,7 @@ def categorize_skewness(skew_value):
         return "stark linksschief"
     elif skew_value > 0.5:
         return "leicht rechtschief"
-    elif skew_value < 0.5:
+    elif skew_value < -0.5:
         return "leicht linksschief"
     else:
         return "eher symmetrisch"
@@ -25,3 +26,29 @@ def shapiro_wilk_test(data,alpha):
         print('Sample looks Gaussian (fail to reject H0)')
     else:
         print('Sample does not look Gaussian (reject H0)')  
+
+def find_similar_rows(df, max_diff=2):
+    # NumPy Array erstellen, um schnell auf die Daten zuzugreifen
+    df_np = df.to_numpy()
+
+    # Ergebnisse zur Speicherung der ähnlichen Zeilen
+    similar_rows = []
+
+    # Iteriere durch jede Zeile und vergleiche mit den anderen Zeilen
+    for i in range(len(df)):
+        # Vergleiche die i-te Zeile mit allen anderen Zeilen
+        for j in range(len(df)):
+            if i != j:  # Vergleiche nur unterschiedliche Zeilen
+                # Berechne die Anzahl der Unterschiede zwischen der i-ten und j-ten Zeile
+                diff_count = np.sum(df_np[i] != df_np[j])
+                
+                # Wenn die Differenz in maximal 2 Spalten liegt, füge die i-te Zeile zu den ähnlichen Zeilen hinzu
+                if diff_count <= max_diff:
+                    similar_rows.append(i)
+                    break  # Wenn eine Übereinstimmung gefunden wurde, überspringe den Rest der Vergleiche für die Zeile
+
+    # Entferne doppelte Zeilenindizes
+    similar_rows = list(set(similar_rows))
+
+    # Gib die gefilterten Zeilen zurück
+    return df.iloc[similar_rows]
